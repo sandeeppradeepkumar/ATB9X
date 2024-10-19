4c06127fbf6cd5f3d597a52e1d04643ac27df7b7
sandeeppradeepkumar <sandeepaycktkumar@gmail.com>
2024-10-19 18:46:03 +0530

    Initial commit

--- src/Oct/ex_02102024/Lab001.java
+++ src/Oct/ex_02102024/Lab001.java
@@ -4,5 +4,6 @@ public class Lab001 {
     public static void main(String[] args)
     {
         System.out.println("Hello World");
+        System.out.println("This is my first JAVA program");
     }
 }
