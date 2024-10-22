06c56c75de2ab3e8299b231c96cfe43600cb2cac
sandeeppradeepkumar <sandeepaycktkumar@gmail.com>
2024-10-23 00:59:22 +0530

    commit

---
+++ src/Oct/ex_16102024/Lab020.java
@@ -0,0 +1,10 @@
+package Oct.ex_16102024;
+
+public class Lab020
+{
+    public static void main(String[] args) {
+        int age=Integer.parseInt(args[0]);
+        System.out.println(age);
+
+    }
+}
---
+++ src/Oct/ex_16102024/Lab021.java
@@ -0,0 +1,13 @@
+package Oct.ex_16102024;
+
+public class Lab021 {
+    public static void main(String[] args) {
+        int age1=Integer.parseInt(args[0]);
+        int age2=Integer.parseInt(args[1]);
+        int age3=Integer.parseInt(args[2]);
+        System.out.println(age1);
+        System.out.println(age2);
+        System.out.println(age3);
+
+    }
+}
